46634fbd55f599a6b1f53d550f4fd444bb5c5ce2
jereme <jereme.daniels@gmail.com>
2019-09-25 15:41:15 -0400

    please work

---
+++ main.js
@@ -0,0 +1,25 @@
+//reusable menu funtion
+function burger(id) {
+    // const burger = document.getElementById('mobile-hamburger');
+    const menu = document.getElementById(id);
+    // body will change any time burger function is used 
+    //unless you add another input variable. 
+    const body = document.getElementById('body');
+    const logo = document.getElementById('logo-div');
+    // const menu = document.getElementById(id);
+    if (menu.style.opacity == "1") {
+        menu.style.opacity = '0';
+        menu.style.pointerEvents = 'none';
+        //new
+        body.style.backgroundImage = 'url("/images/curcuit-board.jpg")';
+        logo.style.opacity = '1';
+        // body.style.backgroundColor = 'white';
+    } else {
+        menu.style.opacity = "1";
+        menu.style.pointerEvents = "auto";
+        // new
+        body.style.backgroundImage = 'none';
+        logo.style.opacity = '0';
+        // body.style.backgroundColor = 'rgba(162, 174, 187, 1)';
+    }
+}
